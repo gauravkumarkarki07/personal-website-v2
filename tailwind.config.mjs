b319bde8557df4d84fcbdf6a81b667f94c30e308
Gaurav Kumar Karki <gauravkumarkarki07@gmail.com>
2024-10-31 23:20:42 +1100

    Basic setup for three windows

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Ubuntu: ["Ubuntu Mono", "sans-serif"],
+      },
+      colors: {
+        background: "#2D2D2D",
+        backgroundOutline: "#5F5F5F",
+      },
+    },
+  },
+  plugins: [],
+};
